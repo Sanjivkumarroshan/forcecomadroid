249e0c1731843a04361bad48e201290a1025044a
lukskrodz <lukskrodz@gmail.com>
2012-03-18 22:21:50 +0000

    working + dist

--- Template/src/pl/skrodzki/android/force/MainActivity.java
+++ Template/src/pl/skrodzki/android/force/MainActivity.java
@@ -1,119 +1,142 @@
-/*
- * Copyright (c) 2011, salesforce.com, inc.
- * All rights reserved.
- * Redistribution and use of this software in source and binary forms, with or
- * without modification, are permitted provided that the following conditions
- * are met:
- * - Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- * - Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- * - Neither the name of salesforce.com, inc. nor the names of its contributors
- * may be used to endorse or promote products derived from this software without
- * specific prior written permission of salesforce.com, inc.
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
 package pl.skrodzki.android.force;
 
-import android.app.Activity;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.salesforce.androidsdk.app.ForceApp;
 import com.salesforce.androidsdk.rest.ClientManager;
 import com.salesforce.androidsdk.rest.ClientManager.LoginOptions;
 import com.salesforce.androidsdk.rest.ClientManager.RestClientCallback;
+import com.salesforce.androidsdk.rest.RestClient.AsyncRequestCallback;
 import com.salesforce.androidsdk.rest.RestClient;
-//import com.salesforce.samples.templateapp.R;
+import com.salesforce.androidsdk.rest.RestRequest;
+import com.salesforce.androidsdk.rest.RestResponse;
+import com.salesforce.androidsdk.util.EventsObservable;
+import com.salesforce.androidsdk.util.EventsObservable.EventType;
 
-/**
- * Main activity
- */
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity{
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-
-		// Setup view
-		setContentView(R.layout.main);
-	}
+    private String[] accounts;
+    private String[] accountIds;
+    private RestClient client;
+    private String apiVersion;
+    
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        ListView lv = getListView();
+        apiVersion = getString(R.string.api_version);
+        
+		lv.setTextFilterEnabled(true);
 	
-	@Override 
-	public void onResume() {
+		lv.setOnItemClickListener(new OnItemClickListener() {
+		    public void onItemClick(AdapterView<?> parent, View view,
+		        int position, long id) {
+	
+		      Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+		          Toast.LENGTH_SHORT).show();
+			  Intent i = new Intent(MainActivity.this, ContactListActivity.class);
+			  i.putExtra("ACCOUNT_ID", accountIds[position]);
+			  startActivity(i);
+		    }
+		});
+	}
+ 
+	@Override
+	public void onResume(){
 		super.onResume();
 		
-		// Hide everything until we are logged in
-		findViewById(R.id.root).setVisibility(View.INVISIBLE);
-		
-		/*
-		 * Un-comment this block to have the passcode screen
-		
-		// Bring up passcode screen if needed
-		ForceApp.APP.getPasscodeManager().lockIfNeeded(this, true);
-		
-		// Do nothing - when the app gets unlocked we will be back here
-		if (ForceApp.APP.getPasscodeManager().isLocked()) {
-			return;
-		}
-		
-		*/
-		
 		// Login options
-		String accountType = ForceApp.APP.getAccountType();
-    	LoginOptions loginOptions = new LoginOptions(
-    			null, // login host is chosen by user through the server picker 
-    			ForceApp.APP.getPasscodeHash(),
-    			getString(R.string.oauth_callback_url),
-    			getString(R.string.oauth_client_id),
-    			new String[] {"api"});
+		String accountType = getString(R.string.account_type);
+		LoginOptions loginOptions = new LoginOptions(
+				null, // gets overridden by LoginActivity based on server picked by uuser 
+				ForceApp.APP.getPasscodeHash(),
+				getString(R.string.oauth_callback_url),
+				getString(R.string.oauth_client_id),
+				new String[] {"api"});
 		
-		// Get a rest client
 		new ClientManager(this, accountType, loginOptions).getRestClient(this, new RestClientCallback() {
 			public void authenticatedRestClient(RestClient client) {
 				if (client == null) {
 					ForceApp.APP.logout(MainActivity.this);
 					return;
 				}
-				
-				// Show everything
-				findViewById(R.id.root).setVisibility(View.VISIBLE);
-
-				// Show welcome
-				((TextView) findViewById(R.id.welcome_text)).setText(getString(R.string.welcome, client.getClientInfo().username));
-				
+				MainActivity.this.client = client;
+				getAlbums();
 			}
 		});
-	}
+	}	
+
 	
-	@Override
-	public void onUserInteraction() {
-		/*
-		 * Un-comment this block if you usethe passcode screen
 
-		ForceApp.APP.getPasscodeManager().recordUserInteraction();
+	private void getAlbums(){
+
+		try {
+			
+			String soql = "select id, name from Account";
+			RestRequest request = RestRequest.getRequestForQuery(apiVersion, soql);
+
+			client.sendAsync(request, new AsyncRequestCallback() {
+
+				public void onSuccess(RestResponse response) {
+					try {
+						if (response == null || response.asJSONObject() == null)
+							return;
+						
+						JSONArray records = response.asJSONObject().getJSONArray("records");
+	
+						if (records.length() == 0)
+							return;
+										
+				    	accounts = new String[records.length()];
+				    	accountIds = new String[records.length()];
+						
+						for (int i = 0; i < records.length(); i++){
+							JSONObject album = (JSONObject)records.get(i);
+							accounts[i] = album.getString("Name");
+							accountIds[i] = album.getString("Id");
+						}
+				        ArrayAdapter<String> ad = new ArrayAdapter<String>(MainActivity.this, 
+				        												   R.layout.list_item, 
+				        												   accounts);
+				        setListAdapter(ad);
+				        EventsObservable.get().notifyEvent(EventType.RenditionComplete);
+					} catch (Exception e) {
+						e.printStackTrace();
+						displayError(e.getMessage());
+					}
+				}
+				
+				public void onError(Exception exception) {
+					displayError(exception.getMessage());
+					EventsObservable.get().notifyEvent(EventType.RenditionComplete);
+				}
+			});
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			displayError(e.getMessage());
+		}
+	}
+
+	
+	private void displayError(String error)	{
+        ArrayAdapter<String> ad = new ArrayAdapter<String>(	MainActivity.this, 
+        													R.layout.list_item, 
+        													new String[]{"Error retrieving Album data - " + error});
+        setListAdapter(ad);
 		
-		*/
 	}
 
-	/**
-	 * Called when "Logout" button is clicked. 
-	 * 
-	 * @param v
-	 */
-	public void onLogoutClick(View v) {
-		ForceApp.APP.logout(this);
-	}
 }
